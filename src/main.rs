88937ea736adbdd044df8434c875e6a925e9270c
fridolin.karger <fridolin.karger@gmail.com>
2024-12-10 20:20:30 +0100

    day 04

--- src/main.rs
+++ src/main.rs
@@ -203,6 +203,145 @@ fn day_3_2() -> i32 {
     }).sum()
 }
 
+fn horizontal_forward_xmas(file: &String) -> i32 {
+    let mut acc = 0;
+    let mut state = 0;
+    for char in file.chars() {
+        match (state, char) {
+            (_, 'X') => state = 1,
+            (1, 'M') => state += 1,
+            (2, 'A') => state += 1,
+            (3, 'S') => {
+                state = 0;
+                acc += 1;
+            },
+            _ => state = 0,
+        }
+    }
+    acc
+}
+
+fn horizontal_backward_xmas(file: &String) -> i32 {
+    horizontal_forward_xmas(&file.chars().rev().collect::<String>())
+}
+
+fn vertical_downwards_xmas(file: &String) -> i32 {
+    let lines = file.lines().collect::<Vec<&str>>();
+    let mut acc = 0;
+    let mut state = 0;
+    for horz_idx in 0..lines[0].len() {
+        for vert_idx in 0..lines.len() {
+            let char = lines[vert_idx].chars().nth(horz_idx).unwrap();
+            match (state, char) {
+                (_, 'X') => state = 1,
+                (1, 'M') => state += 1,
+                (2, 'A') => state += 1,
+                (3, 'S') => {
+                    state = 0;
+                    acc += 1;
+                },
+                _ => state = 0,
+            }
+        }
+    }
+    acc
+}
+
+fn vertical_upwards_xmas(file: &String) -> i32 {
+    let lines = file.lines().collect::<Vec<&str>>();
+    let mut acc = 0;
+    let mut state = 0;
+    for horz_idx in 0..lines[0].len() {
+        for vert_idx in (0..lines.len()).rev() {
+            let char = lines[vert_idx].chars().nth(horz_idx).unwrap();
+            match (state, char) {
+                (_, 'X') => state = 1,
+                (1, 'M') => state += 1,
+                (2, 'A') => state += 1,
+                (3, 'S') => {
+                    state = 0;
+                    acc += 1;
+                },
+                _ => state = 0,
+            }
+        }
+    }
+    acc
+}
+
+
+
+fn day_4_1() -> i32 {
+    let binding = get_file_as_string("input_04");
+    let horizontal_forward = horizontal_forward_xmas(&binding);
+    let mut word = [0; 4];
+    let map = include_bytes!("../input_04")
+        .split(|&c| c == b'\n')
+        .collect::<Vec<_>>();
+
+
+        (0..map[0].len() as isize)
+            .flat_map(|x| (0..map.len() as isize).map(move |y| (x, y)))
+            .flat_map(|(x, y)| {
+                [
+                    [(x, y), (x + 1, y - 1), (x + 2, y - 2), (x + 3, y - 3)], // NE
+                    [(x, y), (x + 1, y), (x + 2, y), (x + 3, y)],             // E
+                    [(x, y), (x + 1, y + 1), (x + 2, y + 2), (x + 3, y + 3)], // SE
+                    [(x, y), (x, y + 1), (x, y + 2), (x, y + 3)],             // S
+                ]
+            })
+            .filter(|coords| {
+                let mut iter = coords.iter().map(|(x, y)| {
+                    map.get(*y as usize)
+                        .and_then(|row| row.get(*x as usize).copied())
+                        .unwrap_or_default()
+                });
+                word.fill_with(|| iter.next().unwrap_or_default());
+                &word == b"XMAS" || &word == b"SAMX"
+            })
+            .count() as i32
+}
+
+fn day_4_2() -> i32 {
+    let mut cross = [0; 4];
+    let map = include_bytes!("../input_04")
+        .split(|&c| c == b'\n')
+        .collect::<Vec<_>>();
+
+
+        (0..map[0].len() as isize)
+            .flat_map(|x| (0..map.len() as isize).map(move |y| (x, y)))
+            .map(|(x, y)| {
+                [
+                    (x + 1, y + 1), // Center
+                    (x, y),         // NE
+                    (x, y + 2),     // SE
+                    (x + 2, y),     // NW
+                    (x + 2, y + 2), // SW
+                ]
+            })
+            .filter(|coords| {
+                let mut iter = coords.iter().map(|(x, y)| {
+                    map.get(*y as usize)
+                        .and_then(|row| row.get(*x as usize).copied())
+                        .unwrap_or_default()
+                });
+
+                if iter.next().is_none_or(|n| n != b'A') {
+                    return false;
+                }
+
+                cross[0] = iter.next().unwrap_or_default();
+                cross[1] = iter.next().unwrap_or_default();
+                cross[2] = iter.next().unwrap_or_default();
+                cross[3] = iter.next().unwrap_or_default();
+
+                &cross == b"MMSS" || &cross == b"MSMS" || &cross == b"SSMM" || &cross == b"SMSM"
+            })
+            .count() as i32
+
+}
+
 fn main() {
     // println!("{}", day_1_1());
     // println!("{}", day_1_2());
@@ -210,13 +349,49 @@ fn main() {
     // println!("{}", day_2_1());
     // println!("{}", day_2_2());
 
-    println!("{}", day_3_1());
-    println!("{}", day_3_2());
+    // println!("{}", day_3_1());
+    // println!("{}", day_3_2());
+
+    // println!("{}", day_4_1());
+    // println!("{}", day_4_2());
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{day_1_1, day_2_1, day_2_2, is_decreasing_true, is_decreasing_true_damp2, is_increasing_true, is_increasing_true_damp2};
+    use crate::{day_2_1, day_2_2, horizontal_backward_xmas, horizontal_forward_xmas, is_decreasing_true, is_decreasing_true_damp2, is_increasing_true, is_increasing_true_damp2, vertical_downwards_xmas, vertical_upwards_xmas};
+
+    const XMAS_TEST: &str =    "MMMSXXMASM
+                                MSAMXMSMSA
+                                AMXSXMAAMM
+                                MSAMASMSMX
+                                XMASAMXAMM
+                                XXAMMXXAMA
+                                SMSMSASXSS
+                                SAXAMASAAA
+                                MAMMMXMMMM
+                                MXMXAXMASX";
+
+    const XMAS_VERT_TEST: &str =   "SXMMMXMMMM\nAMMMMXMMSM\nMAMMMMMMAX\nXSMMMAMMMM\nMMMMMSMMXS";
+
+    #[test]
+    fn test_forward_search() {
+        assert_eq!(horizontal_forward_xmas(&XMAS_TEST.into()), 3);
+    }
+
+    #[test]
+    fn test_backward_search() {
+        assert_eq!(horizontal_backward_xmas(&XMAS_TEST.into()), 2);
+    }
+
+    #[test]
+    fn test_vert_down_search() {
+        assert_eq!(vertical_downwards_xmas(&XMAS_VERT_TEST.into()), 2);
+    }
+
+    #[test]
+    fn test_vert_up_search() {
+        assert_eq!(vertical_upwards_xmas(&XMAS_VERT_TEST.into()), 2);
+    }
 
     #[test]
     fn test_is_decreasing_true() {
